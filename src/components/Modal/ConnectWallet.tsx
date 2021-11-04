01f2f33e6adcb5b45b95de121a552a16bad52197
zhengyimeng <18303592607@163.com>
2021-11-05 00:09:49 +0800

    Add transition

--- src/components/Modal/ConnectWallet.tsx
+++ src/components/Modal/ConnectWallet.tsx
@@ -13,7 +13,10 @@ const ConnectWalletModal = ({ visible, onClose }) => {
   return (
     <Modal visible={visible} onClose={onClose} title={<span className="text-primary text-xl">Connect Wallet</span>}>
       <ConnectWalletModalWarp className="space-x-4 flex items-center flex-wrap justify-center py-10 px-6">
-        <div className="text-center cursor-pointer" onClick={() => login(connectorsByName.walletconnect)}>
+        <div
+          className="text-center cursor-pointer transition duration-200 hover:opacity-80"
+          onClick={() => login(connectorsByName.walletconnect)}
+        >
           <img className="inline-block w-16" src={metamask} alt="metamask" />
           <p className="text-secondaryTextColor text-sm mt-4">Metamask</p>
         </div>
